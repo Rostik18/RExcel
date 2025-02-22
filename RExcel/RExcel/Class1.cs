6ca4461450043ae241ae3182a06c4057df0e2e88
DESKTOP-9LESO28\rosti <rostik1804@gmail.com>
2025-02-22 20:02:28 +0200

    Add empty project

---
+++ RExcel/RExcel/Class1.cs
@@ -0,0 +1,18 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace RExcel
+{
+    public class Class1
+    {
+        private void GenerateWorkbookPartContent(WorkbookPart workbookPart1)
+        {
+            Workbook workbook1 = new Workbook();
+            Sheets sheets1 = new Sheets();
+            Sheet sheet1 = new Sheet() { Name = "Sheet1", SheetId = (UInt32Value)1U, Id = "rId1" };
+            sheets1.Append(sheet1);
+            workbook1.Append(sheets1);
+            workbookPart1.Workbook = workbook1;
+        }
+    }
+}
---
+++ RExcel/RExcel/RWorkbook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RExcel
+{
+    /// <summary> Root element of excel document </summary>
+    public class RWorkbook
+    {
+    }
+}
